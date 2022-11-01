17ce68f3e30ff5292a27ecfc0130ae35ca05a040
BuildTools <unconfigured@null.spigotmc.org>
2022-10-31 20:07:27 -0700

    Switched over to Bulma CSS Framework

--- assets/js/crypto.js
+++ assets/js/crypto.js
@@ -1,14 +1,28 @@
-var apiKey = "coinranking31372a0279a88b1cc0538d1885e1627c3a8da8f9b1e7e1b1"
- var apiUrl = 'https://api.coinranking.com/v2/coins'
- var searchForm = document.getElementById("search-form");
+const myHeaders = new Headers();
 
- function getCoin
+const myRequest = new Request(apiUrl, {
+  method: 'GET',
+  headers: myHeaders,
+  mode: 'cors',
+  cache: 'default',
+});
 
- console.log (apiUrl)
- fetch(apiUrl)
- .then(function(response) {
-     // request was successful
-     if (response.ok) {
-     response.json().then(function(data) {
 
-     console.log(data)
+
+var apiKey = "coinranking31372a0279a88b1cc0538d1885e1627c3a8da8f9b1e7e1b1";
+var apiUrl = 'http://api.coinranking.com/v2/coinranking31372a0279a88b1cc0538d1885e1627c3a8da8f9b1e7e1b1';
+var searchForm = document.getElementById("search-form");
+
+ //function getCoin
+
+console.log (apiUrl);
+fetch(myRequest)
+.then(function(response) {
+    // request was successful
+    console.log(JSON.stringify(response));
+    if (response.ok) {
+        response.json().then(function(data) {
+            console.log(data);
+        })
+    }
+});
