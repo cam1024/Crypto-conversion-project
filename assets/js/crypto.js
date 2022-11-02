133c7ed41b7c9881215dc49b3e38eb9d64d1e8bc
cam1024 <crd9095@yahoo.com>
2022-11-01 20:30:51 -0700

    apiurl

--- assets/js/crypto.js
+++ assets/js/crypto.js
@@ -1,28 +1,29 @@
-const myHeaders = new Headers();
-
-const myRequest = new Request(apiUrl, {
-  method: 'GET',
-  headers: myHeaders,
-  mode: 'cors',
-  cache: 'default',
+const apiKey = 'coinranking31372a0279a88b1cc0538d1885e1627c3a8da8f9b1e7e1b1';
+const url = 'https://api.coinranking.com/v2/coins';
+const resultsContainer = document.getElementById('results');
+const queryString = new URLSearchParams({
+  'x-access-token': apiKey,
+  search: 'Bit',
 });
+// Add the API key to the querystring
+fetch(`${url}?${queryString}`)
+  .then((response) => response.json())
+  .then((response) => {
+    if (response.status === 'success') {
+      const html = response.data.coins.map((coin) => `
+        <tr>
+          <td>${coin.rank}</td>
+          <td>${coin.name}</td>
+          <td>${coin.price}</td>
+        </tr>
+      `);
 
-
-
-var apiKey = "coinranking31372a0279a88b1cc0538d1885e1627c3a8da8f9b1e7e1b1";
-var apiUrl = 'http://api.coinranking.com/v2/coinranking31372a0279a88b1cc0538d1885e1627c3a8da8f9b1e7e1b1';
-var searchForm = document.getElementById("search-form");
-
- //function getCoin
-
-console.log (apiUrl);
-fetch(myRequest)
-.then(function(response) {
-    // request was successful
-    console.log(JSON.stringify(response));
-    if (response.ok) {
-        response.json().then(function(data) {
-            console.log(data);
-        })
+      resultsContainer.innerHTML = html.join('');
+    } else {
+      // Show error message if the request failed
+      resultsContainer.innerHTML = `<tr><td colspan="3">${response.message}</td></tr>`;
     }
-});
+  })
+  .catch((error) => {
+    console.error(error);
+  });
