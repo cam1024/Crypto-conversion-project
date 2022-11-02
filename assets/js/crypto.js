1339ac9bba8e227ddfb55319a7093345c4ba740a
BuildTools <unconfigured@null.spigotmc.org>
2022-11-01 21:10:46 -0700

    updates on crypto.js

--- assets/js/crypto.js
+++ assets/js/crypto.js
@@ -1,28 +1,51 @@
-const myHeaders = new Headers();
-
-const myRequest = new Request(apiUrl, {
-  method: 'GET',
-  headers: myHeaders,
-  mode: 'cors',
-  cache: 'default',
-});
-
-
-
-var apiKey = "coinranking31372a0279a88b1cc0538d1885e1627c3a8da8f9b1e7e1b1";
-var apiUrl = 'http://api.coinranking.com/v2/coinranking31372a0279a88b1cc0538d1885e1627c3a8da8f9b1e7e1b1';
 var searchForm = document.getElementById("search-form");
+var display = document.getElementById("par");
+var coin1 = document.getElementById('coin-1');
+var coin2 = document.getElementById('coin-2');
+var coin3 = document.getElementById('coin-3');
+var coin4 = document.getElementById('coin-4');
+var coin5 = document.getElementById('coin-5');
+var coin6 = document.getElementById('coin-6');
+var coin7 = document.getElementById('coin-7');
+var coin8 = document.getElementById('coin-8');
+var coin9 = document.getElementById('coin-9');
+var coin10 = document.getElementById('coin-10');
 
- //function getCoin
+let p = document.createElement('p');
 
-console.log (apiUrl);
-fetch(myRequest)
-.then(function(response) {
-    // request was successful
-    console.log(JSON.stringify(response));
-    if (response.ok) {
-        response.json().then(function(data) {
-            console.log(data);
-        })
+
+const options = {
+    method: 'GET',
+    headers: {
     }
-});
+};
+
+fetch('https://api.coinlore.net/api/global/', options)
+    .then(response => response.json())
+    .then(response => {
+        console.log(response);
+        p.textContent = 'There are currently ' + response[0].coins_count + ' total Cryptocurrencies.';
+        display.append(p)
+    })
+    .catch(err => console.error(err));
+
+
+    fetch('https://api.coinlore.net/api/tickers/', options)
+    .then(response => response.json())
+    .then(response => {
+        console.log(response);
+        var data = response.data;
+        coin1.textContent = data[0].rank + ': ' + data[0].name + ': $' + data[0].price_usd;
+        coin2.textContent = data[1].rank + ': ' + data[1].name + ': $' + data[1].price_usd;
+        coin3.textContent = data[2].rank + ': ' + data[2].name + ': $' + data[2].price_usd;
+        coin4.textContent = data[3].rank + ': ' + data[3].name + ': $' + data[3].price_usd;
+        coin5.textContent = data[4].rank + ': ' + data[4].name + ': $' + data[4].price_usd;
+        coin6.textContent = data[5].rank + ': ' + data[5].name + ': $' + data[5].price_usd;
+        coin7.textContent = data[6].rank + ': ' + data[6].name + ': $' + data[6].price_usd;
+        coin8.textContent = data[7].rank + ': ' + data[7].name + ': $' + data[7].price_usd;
+        coin9.textContent = data[8].rank + ': ' + data[8].name + ': $' + data[8].price_usd;
+        coin10.textContent = data[9].rank + ': ' + data[9].name + ': $' + data[9].price_usd;
+
+    })
+    .catch(err => console.error(err));
+
